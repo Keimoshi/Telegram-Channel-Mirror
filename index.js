8ac2159cbf5d9aa4c8fa2cad002198c2a44673f0
kai.lu <keimoshi.kai@gmail.com>
2024-10-15 17:52:22 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Custom pathname for the upstream website.
-const channel_name = 'https://t.me/jileyuan51'
-const host_name = 'telegram-channel-mirror.pages.dev'
+const channel_name = 'xiuchedadui'
+const host_name = 'https://t.me/jileyuan51'
 const icon_url = 'https://image.idealclover.cn/blog/assets/icon.png'
 
 const upstream_me = 't.me'
